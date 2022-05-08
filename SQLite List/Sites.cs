6ae2bb6163069e365926d99fd70df20fb966e3a4
Kyle Seidlitz <bodmingaol@gmail.com>
2022-05-08 10:58:01 -0700

    Add project files.

---
+++ SQLite List/Sites.cs
@@ -0,0 +1,13 @@
+﻿namespace SQLite_List
+{
+    public class Sites
+    {
+        public int listID { get; set; }
+        public string? labelName { get; set; }
+        public string? category { get; set; }
+        public string? site { get; set; }
+        public string?  username { get; set; }
+        public string? pass { get; set; }
+        public string? notes { get; set; }
+    }
+}
---
+++ SQLiteDatabase101/UI Form.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace SQLiteDatabase101
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLabel = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboListLabel = new System.Windows.Forms.ComboBox();
+            this.listBAcctList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSite = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLabel
+            // 
+            this.txtLabel.Location = new System.Drawing.Point(473, 57);
+            this.txtLabel.Name = "txtLabel";
+            this.txtLabel.Size = new System.Drawing.Size(162, 23);
+            this.txtLabel.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(560, 415);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.TabStop = false;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(426, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Label:";
+            // 
+            // comboListLabel
+            // 
+            this.comboListLabel.FormattingEnabled = true;
+            this.comboListLabel.Location = new System.Drawing.Point(12, 12);
+            this.comboListLabel.Name = "comboListLabel";
+            this.comboListLabel.Size = new System.Drawing.Size(383, 23);
+            this.comboListLabel.TabIndex = 3;
+            this.comboListLabel.TabStop = false;
+            // 
+            // listBAcctList
+            // 
+            this.listBAcctList.FormattingEnabled = true;
+            this.listBAcctList.ItemHeight = 15;
+            this.listBAcctList.Location = new System.Drawing.Point(14, 48);
+            this.listBAcctList.Name = "listBAcctList";
+            this.listBAcctList.Size = new System.Drawing.Size(381, 319);
+            this.listBAcctList.TabIndex = 4;
+            this.listBAcctList.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Category:";
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(473, 96);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(162, 23);
+            this.txtCategory.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(435, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Site:";
+            // 
+            // txtSite
+            // 
+            this.txtSite.Location = new System.Drawing.Point(473, 134);
+            this.txtSite.Name = "txtSite";
+            this.txtSite.Size = new System.Drawing.Size(162, 23);
+            this.txtSite.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(401, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(473, 175);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(162, 23);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(431, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Pass:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(473, 215);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(162, 23);
+            this.txtPass.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(473, 312);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.TabStop = false;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(554, 312);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 14;
+            this.btnRefresh.TabStop = false;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(554, 341);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.TabStop = false;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(423, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Notes:";
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(473, 255);
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(162, 23);
+            this.txtNotes.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSite);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.listBAcctList);
+            this.Controls.Add(this.comboListLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtLabel);
+            this.Name = "Form1";
+            this.Text = "SQLite Database 103";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtLabel;
+        private Button btnExit;
+        private Label label1;
+        private ComboBox comboListLabel;
+        private ListBox listBAcctList;
+        private Label label2;
+        private TextBox txtCategory;
+        private Label label3;
+        private TextBox txtSite;
+        private Label label4;
+        private TextBox txtUsername;
+        private Label label5;
+        private TextBox txtPass;
+        private Button btnAdd;
+        private Button btnRefresh;
+        private Button btnClear;
+        private Label label6;
+        private TextBox txtNotes;
+    }
+}
